0605ce68067c9074d8f039a686b0c041a3e7b445
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-09-11 22:53:45 +0545

    feat:Working on handleCallAccepted

--- src/screen/RoomPage.jsx
+++ src/screen/RoomPage.jsx
@@ -1,13 +1,73 @@
-import React,{useEffect} from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useSocket } from "../context/SocketProvider";
+import ReactPlayer from "react-player";
+import peer from "../service/peer";
 
 const RoomPage = () => {
-
   const socket = useSocket();
+  const [remoteSocketId, setRemoteSocketId] = useState(null);
+  const [myStream, setMyStream] = useState();
+
+  useEffect(() => {
+    socket.on("user:joined", handleUserJoined);
+    socket.on("incomming:call", handleIncomingCall);
+    socket.on("call:accepted", handleCallAccepted);
+    return () => {
+      socket.off("user:joined", handleUserJoined);
+      socket.off("incomming:call", handleIncomingCall);
+      socket.off("call:accepted", handleCallAccepted);
+    };
+  }, []);
+
+  const handleUserJoined = useCallback(({ email, id }) => {
+    console.log(`email ${email} id ${id}`);
+    setRemoteSocketId(id);
+  }, []);
+
+  const handleIncomingCall = useCallback(
+    async ({ from, offer }) => {
+      setRemoteSocketId(from);
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+      });
+      setMyStream(stream);
+      console.log(`call from ${from} ${offer}`);
+      const ans = await peer.getAnswer(offer);
+      socket.emit("call:accepted", { to: from, ans });
+    },
+    [socket]
+  );
+
+  const handleCallUser = useCallback(async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    const offer = await peer.getOffer();
+    socket.emit("user:call", { to: remoteSocketId, offer });
+    setMyStream(stream);
+  }, [remoteSocketId, socket]);
+
+  const handleCallAccepted = useCallback(async () => {}, []);
 
   return (
     <div>
       <h1>Room Page</h1>
+      <h4>{remoteSocketId ? "Connected" : "No One in Room"}</h4>
+      {remoteSocketId && <button onClick={handleCallUser}>Call</button>}
+      {myStream && (
+        <>
+          <h3>Your Video</h3>
+          <ReactPlayer
+            playing
+            muted
+            url={myStream}
+            height={"300px"}
+            width={"90vw"}
+          />
+        </>
+      )}
     </div>
   );
 };
